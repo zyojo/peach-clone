a763d1993654cac00bc23b341cdea771ffb9dee6
Nakahara Ryota <35990037+zyojo@users.noreply.github.com>
2023-08-25 02:14:24 +0900

    Create a admins/sessions spec

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :admin do
-    email { 'test@example.com' }
+    email { 'admint@example.com' }
     password { 'correct_password' }
   end
 end
--- spec/system/admins/sessions_spec.rb
+++ spec/system/admins/sessions_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe '管理者ログイン', type: :system do
   before do
-    FactoryBot.create(:admin, email: "admin@example.com", password: "password")
+    FactoryBot.create(:admin, email: "admin@example.com", password: "correct_password")
   end
 
-  it 'ログインできること' do
-    visit new_admin_session_path
-    fill_in 'メール', with: 'admin@example.com'
-    fill_in 'パスワード', with: 'password'
-    click_button 'ログイン'
-    expect(page).to have_content '管理者画面'
+  context 'ログイン前' do
+    it '管理画面にアクセスできないこと' do
+      visit admin_root_path
+      expect(current_path).to eq new_admin_session_path
+    end
+
+    it 'ログインできること' do
+      visit new_admin_session_path
+      fill_in 'メール', with: 'admin@example.com'
+      fill_in 'パスワード', with: 'correct_password'
+      click_button 'ログイン'
+      expect(page).to have_content 'ログインしました'
+      expect(current_path).to eq admin_root_path
+    end
   end
 end
