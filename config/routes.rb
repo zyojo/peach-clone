6d67ba11d877c9dadd3b085db9385ad6f1785e70
Nakahara Ryota <35990037+zyojo@users.noreply.github.com>
2023-08-25 03:32:31 +0900

    Add a comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   root to: "tickets#index"
 
-  get "admins", to: "admins/reservations#index", as: :admin_root
+  get "admins", to: "admins/reservations#index", as: :admin_root # ログイン後に遷移するためにasオプションを追加
   
 end
